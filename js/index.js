576bf15e29adba22754edb65f19e6588181e31ed
Aurélien VILMINOT <49270516+aurelien-vilminot@users.noreply.github.com>
2020-04-07 22:30:13 +0200

    Top

---
+++ js/index.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+    $(() => {
+        $(window).on('scroll', () => {
+            if ($(document).scrollTop() > 50) {
+                $('header').addClass("scrolled");
+            } else {
+                $('header').removeClass("scrolled");
+            }
+        });
+        autoWriting();
+    })
+}) ();
+
+function slowScroll(where) {
+    $('html, body').animate({
+        scrollTop: $(where).offset().top}, 1000);
+}
+
+function autoWriting() {
+    let options = {
+        strings: ['Étudiant', 'Développeur', 'Auto-entrepreneur'],
+        typeSpeed: 70,
+        backSpeed: 25,
+        backDelay: 800,
+        startDelay: 1000,
+        showCursor: false,
+        loop: true
+    };
+    new Typed('#autoText', options);
+}
